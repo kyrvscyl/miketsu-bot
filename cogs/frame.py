4988502fb92c8265190b73fe0ecc41b71b804824
kyrvscyl <50376906+patronus-hub@users.noreply.github.com>
2019-06-16 21:53:57 +0800

    corrected channel

--- cogs/frame.py
+++ cogs/frame.py
@@ -27,7 +27,6 @@ class Frame(commands.Cog):
 	async def frame_starlight(self, ctx):
 		server = ctx.guild
 		starlight_role = discord.utils.get(server.roles, name="Starlight Sky")
-		channel = self.client.get_channel(585104196026630185)
 		
 		streak_list = []
 		for user in streak.find({}, {"_id": 0, "user_id": 1, "SSR_current": 1}):
@@ -45,7 +44,7 @@ class Frame(commands.Cog):
 			description = "{}\"s undying luck of not summoning an SSR has earned themselves the Rare Starlight Sky Frame!\n\n:four_leaf_clover: No SSR streak as of posting: {} summons!".format(starlight_new.mention, streak_list_new[0][1]))
 			embed.set_thumbnail(url = "https://vignette.wikia.nocookie.net/onmyoji/images/1/17/Frame7.png")
 			
-			await channel.send(embed=embed)
+			await ctx.channel.send(embed=embed)
 		
 		starlight_current = starlight_role.members[0]
 		
@@ -53,7 +52,7 @@ class Frame(commands.Cog):
 			users.update_one({"user_id": str(starlight_current.id)}, {"$inc": {"jades": 2000}})
 			
 			msg = "{} has earned 2,000{} for wielding the Starlight Sky frame for a day!".format(starlight_current.mention, "<:jade:555630314282811412>")
-			await channel.send(msg)
+			await ctx.channel.send(msg)
 			
 		else:
 			await starlight_new.add_roles(starlight_role)
@@ -65,12 +64,11 @@ class Frame(commands.Cog):
 			description = "{} {} {} the Rare Starlight Sky Frame from {}\"s {}!! {}\n\n:four_leaf_clover: No SSR streak record as of posting: {} summons!".format(starlight_new.mention, random.choice(adverb), random.choice(verb), starlight_current.mention,  random.choice(noun), random.choice(comment), streak_list_new[0][1]))
 			embed.set_thumbnail(url = "https://vignette.wikia.nocookie.net/onmyoji/images/1/17/Frame7.png")
 			
-			await channel.send(embed=embed)
+			await ctx.channel.send(embed=embed)
 					
 	async def frame_blazing(self, ctx):
 		server = ctx.guild
 		blazing_role = discord.utils.get(server.roles, name="Blazing Sun")
-		channel = self.client.get_channel(585104196026630185)
 		
 		ssr_list = []
 		for user in users.find({}, {"_id": 0, "user_id": 1, "SSR": 1}):
@@ -88,7 +86,7 @@ class Frame(commands.Cog):
 			description = "{}\"s fortune luck earned themselves the Rare Blazing Sun Frame!\n\n:four_leaf_clover: Distinct SSRs under possession: {} shikigamis".format(blazing_new.mention, ssr_list_new[0][1]))
 			embed.set_thumbnail(url = "https://vignette.wikia.nocookie.net/onmyoji/images/7/72/Frame62.png")
 		
-			await channel.send(embed=embed)	
+			await ctx.channel.send(embed=embed)	
 		
 		blazing_current = blazing_role.members[0]
 		
@@ -96,7 +94,7 @@ class Frame(commands.Cog):
 			users.update_one({"user_id": str(blazing_current.id)}, {"$inc": {"amulets": 10}})
 			
 			msg = "{} has earned 10{} for wielding the Blazing Sun frame for a day!".format(blazing_current.mention, "<:amulet:573071120685596682>")
-			await channel.send(msg)
+			await ctx.channel.send(msg)
 		
 		else :
 			await blazing_new.add_roles(blazing_role)
@@ -107,7 +105,7 @@ class Frame(commands.Cog):
 			embed = discord.Embed(color=0xffff80, title=":incoming_envelope: Hall of Framers Update",
 			description = "{} {} {} the Rare Blazing Sun Frame from {}\"s {}!! {}\n\n:four_leaf_clover: Distinct SSRs under possession: {} shikigamis".format(blazing_new.mention, random.choice(adverb), random.choice(verb), blazing_current.mention, random.choice(noun), random.choice(comment), ssr_list_new[0][1]))
 			embed.set_thumbnail(url="https://vignette.wikia.nocookie.net/onmyoji/images/7/72/Frame62.png")
-			await channel.send(embed=embed)
+			await ctx.channel.send(embed=embed)
 			
 def setup(client):
 	client.add_cog(Frame(client))
