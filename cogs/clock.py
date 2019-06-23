a8544f9dc32eb5a8e2974cc19cf335a7ce2517b0
kyrvscyl <51079653+kyrvscyl@users.noreply.github.com>
2019-06-23 18:54:54 +0800

    Integrated "show me your patronus" quest

--- cogs/clock.py
+++ cogs/clock.py
@@ -3,13 +3,18 @@ Discord Miketsu Bot.
 kyrvscyl, 2019
 """
 import asyncio
+import random
 from datetime import datetime
 
 import discord
 import pytz
-from discord.ext import tasks, commands
+from discord.ext import commands
 
-from cogs.mongo.db import books
+from cogs.magic import Magic
+from cogs.magic import get_data
+from cogs.mongo.db import books, weather, sendoff, quests, owls
+
+print(Magic)
 
 # Get the clock channels
 clock_channels = []
@@ -31,35 +36,147 @@ def get_time():
     return time
 
 
-# noinspection PyCallingNonCallable,PyShadowingNames
+# noinspection PyCallingNonCallable
 class Clock(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
+    async def logging(self, msg):
+        channel = self.client.get_channel(592270990894170112)
+        await channel.send(msg)
+
     @commands.Cog.listener()
     async def on_ready(self):
-        self.clock_update.start()
 
-    @tasks.loop(seconds=30)
+        while True:
+            if get_time().strftime("%S") == "00":
+                await self.clock_update()
+                await asyncio.sleep(1)
+            else:
+                await asyncio.sleep(1)
+
+    # noinspection PyShadowingNames
+    async def penalty_hour(self):
+        quests.update_many({"quest1.status": "ongoing"}, {"$inc": {"quest1.$.score": -2}})
+        await self.logging("Penalizing everyone with 2 points for every hour passed")
+
+    async def owls_restock(self):
+        owls.update_many({"purchaser": {"$ne": "None"}}, {"$set": {"purchaser": "None"}})
+        await self.logging("Restocking emporium with all owls")
+
+    async def actions_reset(self):
+        quests.update_many({"quest1.status": "ongoing"}, {"$set": {"quest1.$.actions": 0}})
+        await self.logging("Resetting all user actions to 0")
+
+    async def send_off_complete(self):
+
+        for entry in sendoff.find({"timestamp_complete": {"$exists": True}}, {"_id": 0}):
+            if entry["timestamp_complete"] == get_time().strftime("%Y-%b-%d %HH"):
+
+                user = self.client.get_user(int(entry["user_id"]))
+                cycle, path, timestamp, user_hint, actions = get_data(user)
+
+                if entry["scenario"] == 2:
+                    description = f"Dear {user.name},\n\nIt is wondrous to have such another promising magic " \
+                        f"practitioner, you see in the castle everyone there is special, but what makes them " \
+                        f"more special is their Patronus charm.\n\nYour term awaits soon. Good luck!\n\n" \
+                        f"Yours Truly,\nThe Headmaster "
+
+                    embed = discord.Embed(color=0xffff80, description=description)
+                    msg = f"Your {entry['type']} has returned with a letter from the Headmaster"
+                    await Magic(self).update_path(user, cycle, path_new="path3")
+                    await user.send(msg, embed=embed)
+                    await self.logging(f"Sent {user}: Confirmation letter receipt from the Headmaster")
+
+                elif entry["scenario"] == 1:
+                    user = self.client.get_user(int(entry["user_id"]))
+                    msg = f"Your {entry['type']} has fully recovered"
+
+                    await Magic(self).update_path(user, cycle, path_new="path20")
+                    await user.send(embed=msg)
+
+    async def send_off_report(self):
+
+        for entry in sendoff.find({"timestamp_update": {"$exists": True}}, {"_id": 0}):
+            if entry["timestamp_update"] == get_time().strftime("%Y-%b-%d %HH"):
+                user = self.client.get_user(int(entry["user_id"]))
+
+                if entry["scenario"] == 1:
+                    cycle, path, timestamp, user_hint, actions = get_data(user)
+                    await Magic(self).penalize(user, cycle, points=20)
+
+                await user.send(entry["report"])
+
+    async def clear_secrets(self):
+        for entry in books.find({}, {"_id": 0, "eeylops-owl-emporium": 1, "ollivanders": 1, "gringotts-bank": 1}):
+            for secret in entry:
+                for key in entry[secret]:
+                    if key == "id":
+                        channel = self.client.get_channel(int(entry[secret][key]))
+                        if channel is not None:
+                            await channel.delete()
+
     async def clock_update(self):
+        time = get_time().strftime("%H:%M:%S EST | %a")
+        hour_minute = datetime.now(tz=tz_target).strftime("%H:%M")
+        minute = datetime.now(tz=tz_target).strftime("%M")
+        hour_24 = int(datetime.now(tz=tz_target).strftime("%H"))
+        hour_12 = datetime.now(tz=tz_target).strftime("%I")
 
         if status == "None":
-            hour = datetime.now(tz=tz_target).strftime("%I")
-            minute = datetime.now(tz=tz_target).strftime("%M")
+            weather1 = weather.find_one({"weather1": {"$type": "string"}}, {"weather1": 1})["weather1"]
+            weather2 = weather.find_one({"weather2": {"$type": "string"}}, {"weather2": 1})["weather2"]
+
+            # noinspection PyShadowingNames
+            def generate_weather(hour):
+                if 18 > hour >= 6:
+                    day = weather.find_one({"type": "day"}, {"_id": 0, "type": 0})
+                    weather1 = random.choice(list(day.values()))
+                    weather2 = ""
+                    weather.update_one({"weather1": {"$type": "string"}}, {"$set": {"weather1": weather1}})
+                    weather.update_one({"weather2": {"$type": "string"}}, {"$set": {"weather2": weather2}})
+                else:
+                    night = weather.find_one({"type": "night"}, {"_id": 0, "type": 0})
+                    moon = weather.find_one({"type": "moon"}, {"_id": 0, "type": 0})
+                    weather1 = random.choice(list(night.values()))
+                    weather2 = random.choice(list(moon.values()))
+                    weather.update_one({"weather1": {"$type": "string"}}, {"$set": {"weather1": weather1}})
+                    weather.update_one({"weather2": {"$type": "string"}}, {"$set": {"weather2": weather2}})
+
+                return weather1, weather2
+
+            if minute == "00":
+                weather1, weather2 = generate_weather(hour_24)
+                await self.penalty_hour()
+                await self.actions_reset()
+                await self.clear_secrets()
+                await self.send_off_report()
+                await self.send_off_complete()
+
+            if hour_minute == "00:00":
+                await self.owls_restock()
 
             for clock_channel in clock_channels:
                 clock = self.client.get_channel(int(clock_channel))
 
-                def get_emoji(hour, minute):
-                    if int(minute) >= 30:
-                        emoji_clock_index = int(hour) * 2 + 1
-                    else:
-                        emoji_clock_index = int(hour) * 2
-                    emoji_clock = list_clock[int(emoji_clock_index)]
-                    return emoji_clock
+                if clock is not None:
 
-                await clock.edit(name=f"{get_emoji(hour, minute)} {get_time().strftime('%H:%M EST | %a')}")
+                    # noinspection PyShadowingNames
+                    def get_emoji(hour, minute):
+                        if int(minute) >= 30:
+                            emoji_clock_index = int(hour) * 2 + 1
+                        else:
+                            emoji_clock_index = int(hour) * 2
+                        emoji_clock = list_clock[int(emoji_clock_index)]
+                        return emoji_clock
+
+                    name = f"{get_emoji(hour_12, minute)} {time}"
+
+                    if clock_channel == "584975951788638228":
+                        name = f"{get_emoji(hour_12, minute)} {time} {weather1} {weather2}"
+
+                    await clock.edit(name=name)
 
     @commands.command(aliases=["so"])
     @commands.cooldown(1, 900, commands.BucketType.guild)
