92c065b56f898ce77051c251c4ff703902f25c44
kyrvscyl <51079653+kyrvscyl@users.noreply.github.com>
2019-07-03 12:52:13 +0800

    added error handling and perform minimalization

--- cogs/raid.py
+++ cogs/raid.py
@@ -30,38 +30,37 @@ def calculate(x, y, z):
 
 
 async def raid_giverewards_victim_as_winner(victim, raider):
-    # Victim
-    users.update_one({"user_id": str(victim.id)}, {"$inc": {"coins": 50000, "jades": 100, "medals": 20}})
 
-    # Raider
+    users.update_one({"user_id": str(victim.id)}, {"$inc": {"coins": 50000, "jades": 100, "medals": 50}})
     users.update_one({"user_id": str(raider.id)}, {"$inc": {"realm_ticket": -1}})
 
-    # No negative medals
     if users.find_one({"user_id": str(raider.id)}, {"_id": 0})["medals"] < 10:
         users.update_one({"user_id": str(raider.id)}, {"$set": {"medals": 0}})
+
     else:
         users.update_one({"user_id": str(raider.id)}, {"$inc": {"medals": -10}})
 
 
 async def raid_giverewards_raider_as_winner(victim, raider):
-    # Raider
-    users.update_one({"user_id": str(raider.id)},
-                     {"$inc": {"coins": 25000, "jades": 30, "medals": 10, "realm_ticket": -1}})
 
-    # No negative medals
+    users.update_one({"user_id": str(raider.id)},
+                     {"$inc": {"coins": 25000, "jades": 50, "medals": 25, "realm_ticket": -1}})
+
     if users.find_one({"user_id": str(victim.id)}, {"_id": 0})["medals"] < 10:
         users.update_one({"user_id": str(victim.id)}, {"$set": {"medals": 0}})
+
     else:
         users.update_one({"user_id": str(victim.id)}, {"$inc": {"medals": -10}})
 
 
 async def raid_calculation(victim, raider, ctx):
+
     try:
-        # Getting profiles
-        profile_raider = users.find_one({'user_id': str(raider.id)},
-                                        {'_id': 0, 'level': 1, 'medals': 1, 'SP': 1, 'SSR': 1, 'SR': 1, 'R': 1})
-        profile_victim = users.find_one({'user_id': str(victim.id)},
-                                        {'_id': 0, 'level': 1, 'medals': 1, 'SP': 1, 'SSR': 1, 'SR': 1, 'R': 1})
+
+        profile_raider = users.find_one({"user_id": str(raider.id)},
+                                        {"_id": 0, "level": 1, "medals": 1, "SP": 1, "SSR": 1, "SR": 1, "R": 1})
+        profile_victim = users.find_one({"user_id": str(victim.id)},
+                                        {"_id": 0, "level": 1, "medals": 1, "SP": 1, "SSR": 1, "SR": 1, "R": 1})
 
         chance1 = calculate(profile_raider["level"], profile_victim["level"], 0.15)
         chance2 = calculate(profile_raider["medals"], profile_victim["medals"], 0.15)
@@ -71,22 +70,21 @@ async def raid_calculation(victim, raider, ctx):
         chance6 = calculate(profile_raider["R"], profile_victim["R"], 0.01)
         total_chance = round((0.5 + chance1 + chance2 + chance3 + chance4 + chance5 + chance6) * 100, 2)
 
-        embed = discord.Embed(color=raider.colour, title=":gear: Raid Calculation",
-                              description=f":bow_and_arrow: Raider: `{raider.name}`\n"
-                              f":dart:  Victim: `{victim.name}`\n\n"
-                              f":game_die: `{total_chance}%` success")
-        embed.set_thumbnail(url=raider.avatar_url)
+        embed = discord.Embed(color=raider.colour)
+        embed.set_author(icon_url=raider.avatar_url, name=f"vs {victim.display_name} :: {total_chance}%")
         await ctx.channel.send(embed=embed)
 
-    # Invalid mentioned user
     except KeyError:
-        await ctx.channel.send(f"{raider.mention}, I did not find that user. Please try again.")
+        return
+
     except TypeError:
         return
 
 
 async def raid_attack(victim, raider, ctx):
+
     try:
+
         profile_raider = users.find_one({'user_id': str(raider.id)},
                                         {'_id': 0, 'level': 1, 'medals': 1, 'SP': 1, 'SSR': 1, 'SR': 1, 'R': 1})
         profile_victim = users.find_one({'user_id': str(victim.id)},
@@ -102,33 +100,31 @@ async def raid_attack(victim, raider, ctx):
         roll = random.uniform(0, 100)
 
         if roll <= total_chance:
-            embed = discord.Embed(color=raider.colour, title=":clipboard: Raid Report",
-                                  description=f":bow_and_arrow: Raider: `{raider.name}`\n"
-                                  f":dart: Victim: `{victim.name}`\n\n:game_die: `{total_chance}%` success\n"
-                                  f":trophy: `{raider.name}` prevails!\n\n"
-                                  f":gift: Rewards:\n25,000{emoji_c}, 30{emoji_j}, 10{emoji_m}")
-            embed.set_thumbnail(url=raider.avatar_url)
+            embed = discord.Embed(color=raider.colour)
+            embed.set_author(icon_url=raider.avatar_url, name=f"vs {victim.display_name} :: {total_chance}%")
+            embed.add_field(name=f"*Result: {raider.display_name} wins!*",
+                            value=f"25,000{emoji_c}, 50{emoji_j}, 25{emoji_m}")
 
             await raid_giverewards_raider_as_winner(victim, raider)
             await ctx.channel.send(embed=embed)
+
         else:
-            embed = discord.Embed(color=victim.colour, title=":clipboard: Raid Report",
-                                  description=f":bow_and_arrow: Raider: `{raider.name}`\n"
-                                  f":dart:  Victim: `{victim.name}`\n\n:game_die: `{total_chance}%` success\n"
-                                  f":trophy: `{victim.name}` prevails!\n\n"
-                                  f":gift: Comeback Rewards:\n50,000{emoji_c}, 100{emoji_j}, 20{emoji_m}")
-            embed.set_thumbnail(url=victim.avatar_url)
+            embed = discord.Embed(color=raider.colour)
+            embed.set_author(icon_url=raider.avatar_url, name=f"vs {victim.display_name} :: {total_chance}%")
+            embed.add_field(name=f"*Result: {victim.display_name} wins!*",
+                            value=f"50,000{emoji_c}, 100{emoji_j}, 50{emoji_m}")
 
             await raid_giverewards_victim_as_winner(victim, raider)
             await ctx.channel.send(embed=embed)
 
-    # Invalid mentioned user
     except KeyError:
-        await ctx.channel.send(f"{raider.mention}, I did not find that user. Please try again.")
+        return
+
     except TypeError:
         return
 
 
+# noinspection PyTypeChecker
 class Startup(commands.Cog):
 
     def __init__(self, client):
@@ -136,63 +132,51 @@ class Startup(commands.Cog):
 
     @commands.command(aliases=["r"])
     @commands.guild_only()
-    async def raid(self, ctx, victim: discord.User = None):
+    async def raid(self, ctx, *, victim: discord.Member = None):
         raider = ctx.author
 
-        # No mentioned user
         if victim is None:
-            await ctx.channel.send(f"{ctx.author.mention}, please mention a user to raid. Consumes 1:tickets:")
+            await ctx.channel.send(f"{raider.mention}, mention a user to raid")
 
-        # No Victim is a bot
-        elif victim.bot:
-            await ctx.channel.send(f"{ctx.author.mention}, you cannot do that!")
+        elif victim.bot or victim.id == ctx.author.id:
+            return
 
-        # Victim is self
-        elif victim.id == ctx.author.id:
-            await ctx.channel.send(f"{ctx.author.mention}, you cannot raid your own realm.")
-
-        # Checking realm tickets
         elif users.find_one({"user_id": str(raider.id)}, {"_id": 0, "realm_ticket": 1})["realm_ticket"] == 0:
-            await ctx.channel.send(f"{ctx.author.mention}, You have insufficient :tickets:")
+            await ctx.channel.send(f"{raider.mention}, you have insufficient :tickets:")
 
-        # Create initial record
         elif daily.find_one({"key": "raid"}, {"_id": 0, f"{victim.id}.raid_count": 1}) == {}:
             daily.update_one({"key": "raid"}, {"$set": {f"{victim.id}.raid_count": 1}})
             await raid_attack(victim, raider, ctx)
 
-        # Employ protection
         elif daily.find_one({"key": "raid"},
                             {"_id": 0, f"{victim.id}.raid_count": 1})[str(victim.id)]["raid_count"] == 3:
-            await ctx.channel.send(f"{ctx.author.mention}, this user's realm is under protection.")
+            await ctx.channel.send(f"{raider.mention}, this user's realm is under protection.")
 
-        # Raids the victim
         elif daily.find_one({"key": "raid"},
                             {"_id": 0, f"{victim.id}.raid_count": 1})[str(victim.id)]["raid_count"] < 4:
             daily.update_one({"key": "raid"}, {"$inc": {f"{victim.id}.raid_count": 1}})
             await raid_attack(victim, raider, ctx)
 
     @commands.command(aliases=["rc", "raidc"])
-    async def raid_calculate(self, ctx, user: discord.User = None):
+    async def raid_calculate(self, ctx, *, victim: discord.Member = None):
 
-        # No mentioned user
-        if user is None:
-            embed = discord.Embed(color=0xffff80, title=":game_die: Raid Calculation",
-                                  description="This calculates the percent chance of successfully raiding the "
-                                              "mentioned user. Success chance is a function of level, medals, "
-                                              "& shikigami pool of the victim & raider.\n\n:white_small_square: Base "
-                                              "chance: 50%\n:white_small_square: Level: ± 15%\n:white_small_square: "
-                                              "Medals: ± 15%\n:white_small_square: SP: ± 9%\n:white_small_square: SSR: "
-                                              "± 7%\n:white_small_square: SR: ±3%\n:white_small_square: R: ±1%")
-            embed.set_thumbnail(url=self.client.user.avatar_url)
+        if victim is None:
+            embed = discord.Embed(color=ctx.author.colour,
+                                  title="Success Chance",
+                                  description="• Base chance: 50%\n"
+                                              "• Level: ± 15%\n"
+                                              "• Medals: ± 15%\n"
+                                              "• SP: ± 9%\n"
+                                              "• SSR: ± 7%\n"
+                                              "• SR: ± 3%\n"
+                                              "• R: ± 1%")
             await ctx.channel.send(embed=embed)
 
-        # Responds on self raid
-        elif user == ctx.author or user.bot is True:
-            await ctx.channel.send(f"{ctx.author.mention}, why would you do that?")
+        elif victim == ctx.author or victim.bot is True:
+            return
 
-        # Calculates the chance
-        elif user != ctx.author:
-            await raid_calculation(user, ctx.author, ctx)
+        elif victim != ctx.author:
+            await raid_calculation(victim, ctx.author, ctx)
 
 
 def setup(client):
