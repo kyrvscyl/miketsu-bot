b6d0dcaf35a28886fd6e4ec0c2b97a7c5ff874de
kyrvscyl <51079653+kyrvscyl@users.noreply.github.com>
2019-07-05 15:01:38 +0800

    remove daily boss reset, conduct mass optimization

--- cogs/clock.py
+++ cogs/clock.py
@@ -14,7 +14,7 @@ from cogs.magic import Magic
 from cogs.magic import get_data
 from cogs.mongo.db import books, weather, sendoff, quests, owls
 from cogs.frame import frame_starlight, frame_blazing
-from cogs.admin import Admin
+from cogs.admin import Admin, reset_boss
 from cogs.automation import Events
 
 # Get the clock channels
@@ -62,6 +62,38 @@ def get_time():
     return time
 
 
+# noinspection PyShadowingNames
+def generate_weather(hour):
+
+    if 18 > hour >= 6:
+        day = weather.find_one({"type": "day"}, {"_id": 0, "type": 0})
+        weather1 = random.choice(list(day.values()))
+        weather2 = ""
+        weather.update_one({"weather1": {"$type": "string"}}, {"$set": {"weather1": weather1}})
+        weather.update_one({"weather2": {"$type": "string"}}, {"$set": {"weather2": weather2}})
+
+    else:
+        night = weather.find_one({"type": "night"}, {"_id": 0, "type": 0})
+        moon = weather.find_one({"type": "moon"}, {"_id": 0, "type": 0})
+        weather1 = random.choice(list(night.values()))
+        weather2 = random.choice(list(moon.values()))
+        weather.update_one({"weather1": {"$type": "string"}}, {"$set": {"weather1": weather1}})
+        weather.update_one({"weather2": {"$type": "string"}}, {"$set": {"weather2": weather2}})
+
+    return weather1, weather2
+
+
+def get_emoji(hours, minutes):
+    if int(minutes) >= 30:
+        emoji_clock_index = (int(hours) * 2) + 2
+
+    else:
+        emoji_clock_index = (int(hours) * 2) + 1
+
+    emoji_clock = list_clock[emoji_clock_index]
+    return emoji_clock
+
+
 # noinspection PyCallingNonCallable
 class Clock(commands.Cog):
 
@@ -71,6 +103,7 @@ class Clock(commands.Cog):
     async def logging(self, msg):
         channel = self.client.get_channel(592270990894170112)
         date_time = datetime.now(tz=tz_target).strftime("%Y-%b-%d %HH")
+
         await channel.send(f"[{date_time}] " + msg)
 
     @commands.Cog.listener()
@@ -80,6 +113,7 @@ class Clock(commands.Cog):
             if get_time().strftime("%S") == "00":
                 await self.clock_update()
                 await asyncio.sleep(1)
+
             else:
                 await asyncio.sleep(1)
 
@@ -156,6 +190,7 @@ class Clock(commands.Cog):
                     msg2 = "*\"It was your owl flocking gracefully with its wings " \
                            "and holding a paper with its feet.\"*"
                     msg3 = f"*\"Your {entry['type'].capitalize()} owl has returned with a letter from the Headmaster\"*"
+
                     await user.send(msg1)
                     await asyncio.sleep(4)
                     await user.send(msg2)
@@ -168,6 +203,7 @@ class Clock(commands.Cog):
             elif entry["scenario"] == 1:
                 user = self.client.get_user(int(entry["user_id"]))
                 msg = f"Your {entry['type']} has fully recovered"
+
                 await Magic(self.client).update_path(user, cycle, path_new="path20")
                 await user.send(msg)
 
@@ -182,8 +218,14 @@ class Clock(commands.Cog):
                     await Magic(self.client).penalize(user, cycle, points=20)
 
                 description = entry["report"]
-                embed = discord.Embed(color=0xffffff, title="Owl Report", description=description)
+
+                embed = discord.Embed(
+                    color=0xffffff,
+                    title="Owl Report",
+                    description=description
+                )
                 embed.set_footer(text=f"{entry['timestamp_update']}")
+
                 await user.send(embed=embed)
 
     async def reset_purchase(self):
@@ -191,18 +233,27 @@ class Clock(commands.Cog):
         await self.logging("Resetting everyone's ability to purchase owls to True")
 
     async def clear_secrets(self):
-        for entry in books.find({}, {"_id": 0, "eeylops-owl-emporium": 1, "ollivanders": 1, "gringotts-bank": 1}):
+
+        query = books.find({}, {
+            "_id": 0, "eeylops-owl-emporium": 1, "ollivanders": 1, "gringotts-bank": 1
+        })
+
+        for entry in query:
             for secret in entry:
                 for key in entry[secret]:
+
                     if key == "id":
                         channel = self.client.get_channel(int(entry[secret][key]))
+
                         if channel is not None:
                             try:
                                 await channel.delete()
+
                             except discord.errors.Forbidden:
                                 return
 
     async def clock_update(self):
+
         time = get_time().strftime("%H:%M EST | %a")
         hour_minute = datetime.now(tz=tz_target).strftime("%H:%M")
         minute = datetime.now(tz=tz_target).strftime("%M")
@@ -212,24 +263,6 @@ class Clock(commands.Cog):
         if status == "None":
             weather1 = weather.find_one({"weather1": {"$type": "string"}}, {"weather1": 1})["weather1"]
             weather2 = weather.find_one({"weather2": {"$type": "string"}}, {"weather2": 1})["weather2"]
-
-            # noinspection PyShadowingNames
-            def generate_weather(hour):
-                if 18 > hour >= 6:
-                    day = weather.find_one({"type": "day"}, {"_id": 0, "type": 0})
-                    weather1 = random.choice(list(day.values()))
-                    weather2 = ""
-                    weather.update_one({"weather1": {"$type": "string"}}, {"$set": {"weather1": weather1}})
-                    weather.update_one({"weather2": {"$type": "string"}}, {"$set": {"weather2": weather2}})
-                else:
-                    night = weather.find_one({"type": "night"}, {"_id": 0, "type": 0})
-                    moon = weather.find_one({"type": "moon"}, {"_id": 0, "type": 0})
-                    weather1 = random.choice(list(night.values()))
-                    weather2 = random.choice(list(moon.values()))
-                    weather.update_one({"weather1": {"$type": "string"}}, {"$set": {"weather1": weather1}})
-                    weather.update_one({"weather2": {"$type": "string"}}, {"$set": {"weather2": weather2}})
-
-                return weather1, weather2
 
             # Reset items during every after hour
             if minute == "00":
@@ -243,20 +276,18 @@ class Clock(commands.Cog):
                 await self.send_off_complete()
                 await Events(self.client).reset_prefects()
 
-            # Reset owl during 12:00
-            if hour_minute == "12:00":
+            if hour_minute in ["00:00", "06:00", "12:00", "18:00"]:
                 await self.owls_restock()
 
-            # Reset items during every after day
             if hour_minute == "00:00":
                 server = self.client.get_guild(412057028887052288)
                 spell_spam = self.client.get_channel(417507997846339585)
+
                 await Admin(self.client).reset_daily(spell_spam)
-                await Admin(self.client).reset_boss(spell_spam)
+                await reset_boss(spell_spam)
                 await frame_starlight(server, spell_spam)
                 await frame_blazing(server, spell_spam)
 
-            # Start transformation
             if hour_minute == "19:00":
                 await self.transformation_start()
 
@@ -266,22 +297,16 @@ class Clock(commands.Cog):
             for clock_channel in clock_channels:
                 clock = self.client.get_channel(int(clock_channel))
 
-                def get_emoji(hours, minutes):
-                    if int(minutes) >= 30:
-                        emoji_clock_index = (int(hours) * 2) + 2
-                    else:
-                        emoji_clock_index = (int(hours) * 2) + 1
-                    emoji_clock = list_clock[emoji_clock_index]
-                    return emoji_clock
-
                 if clock_channel == "584975951788638228":
                     name = f"{get_emoji(hour_12, minute)} {time} {weather1} {weather2}"
+
                 else:
                     name = f"{get_emoji(hour_12, minute)} {time}"
 
                 if clock is not None:
                     try:
                         await clock.edit(name=name)
+
                     except discord.errors.Forbidden:
                         return
 
@@ -290,10 +315,14 @@ class Clock(commands.Cog):
     async def shoutout(self, ctx, *args):
 
         if len(args) == 0:
-            embed = discord.Embed(color=0xffff80,
-                                  description="Temporarily change the clock to a custom message.\n\nUse `;shoutout "
-                                              "<duration: 1-10 min> <message: 25 character limit`")
+
+            embed = discord.Embed(
+                color=ctx.author.colour,
+                description="Temporarily change the clock to a custom message.\n\n"
+                            "Use `;shoutout <duration: 1-10 min> <message: 25 character limit`"
+            )
             embed.set_thumbnail(url=self.client.user.avatar_url)
+
             await ctx.channel.send(embed=embed)
             self.client.get_command("shoutout").reset_cooldown(ctx)
 
@@ -310,6 +339,7 @@ class Clock(commands.Cog):
             self.client.get_command("shoutout").reset_cooldown(ctx)
 
         elif abs(int(args[0])) <= 10 and len(" ".join(args[1::])) <= 25:
+
             msg = " ".join(args[1::])
             clock_channel = books.find_one({"server": str(ctx.guild.id)}, {"clock": 1, "_id": 0})["clock"]
             clock = self.client.get_channel(int(clock_channel))
