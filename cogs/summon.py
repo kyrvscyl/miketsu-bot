415400cfec29f5616d536dcd7a2b7b0afc5634b5
kyrvscyl <51079653+kyrvscyl@users.noreply.github.com>
2019-07-08 14:32:14 +0800

    optimize summon.py

--- cogs/summon.py
+++ cogs/summon.py
@@ -40,45 +40,80 @@ caption.close()
 
 
 def pluralize(singular, count):
+
     if count > 1:
         return singular + "s"
+
     else:
         return singular
 
 
 # noinspection PyShadowingNames
 async def summon_update(user, users, sum_sp, sum_ssr, sum_sr, sum_r, amulet_pull, summon_pull):
-    users.update_one({"user_id": str(user.id)}, {
-        "$inc": {"SP": sum_sp, "SSR": sum_ssr, "SR": sum_sr, "R": sum_r, "amulets_spent": amulet_pull,
-                 "amulets": -amulet_pull}})
+
+    users.update_one({
+        "user_id": str(user.id)}, {
+        "$inc": {
+            "SP": sum_sp,
+            "SSR": sum_ssr,
+            "SR": sum_sr,
+            "R": sum_r,
+            "amulets_spent": amulet_pull,
+            "amulets": -amulet_pull
+        }
+    })
 
     for summon in summon_pull:
-        # Creates a shikigami profile
-        if users.find_one({"user_id": str(user.id), "shikigami.name": summon[1].replace("||", "")},
-                          {"_id": 0, "shikigami.$": 1}) is None:
-            users.update_one({"user_id": str(user.id)}, {"$push": {
-                "shikigami": {"name": summon[1].replace("||", ""), "rarity": summon[0], "grade": 1, "owned": 0,
-                              "evolved": "False"}
-            }})
 
-        users.update_one({"user_id": str(user.id), "shikigami.name": summon[1].replace("||", "")}, {
+        query = users.find_one({
+            "user_id": str(user.id),
+            "shikigami.name": summon[1].replace("||", "")}, {
+            "_id": 0, "shikigami.$": 1
+        })
+
+        if query is None:
+
+            users.update_one({
+                "user_id": str(user.id)}, {
+                "$push": {
+                    "shikigami": {
+                        "name": summon[1].replace("||", ""),
+                        "rarity": summon[0],
+                        "grade": 1,
+                        "owned": 0,
+                        "evolved": "False"}
+                }
+            })
+
+        users.update_one({
+            "user_id": str(user.id),
+            "shikigami.name": summon[1].replace("||", "")}, {
             "$inc": {
                 "shikigami.$.owned": 1
-            }})
+            }
+        })
 
 
 async def summon_streak(user, summon_pull):
+
     if streak.find_one({"user_id": str(user.id)}, {"_id": 0}) is None:
-        profile = {"user_id": str(user.id), "SSR_current": 0, "SSR_record": 0}
+
+        profile = {
+            "user_id": str(user.id),
+            "SSR_current": 0, "SSR_record": 0
+        }
         streak.insert_one(profile)
 
     for summon in summon_pull:
+
         ssr_current = streak.find_one({"user_id": str(user.id)}, {"_id": 0})["SSR_current"]
         ssr_record = streak.find_one({"user_id": str(user.id)}, {"_id": 0})["SSR_record"]
 
         if summon[0] == "SP" or summon[0] == "SR" or summon[0] == "R":
+
             if ssr_current == ssr_record:
                 streak.update_one({"user_id": str(user.id)}, {"$inc": {"SSR_current": 1, "SSR_record": 1}})
+
             else:
                 streak.update_one({"user_id": str(user.id)}, {"$inc": {"SSR_current": 1}})
 
@@ -92,14 +127,19 @@ async def summon_perform(ctx, user, amulet_pull):
 
     for count in range(amulet_pull):
         roll = random.uniform(0, 100)
+
         if roll < 1.2:
             p = random.uniform(0, 1.2)
+
             if p >= 126 / 109:
                 summon_pull.append(("SP", "||{}||".format(random.choice(pool_sp))))
+
             else:
                 summon_pull.append(("SSR", "||{}||".format(random.choice(pool_ssr))))
+
         elif roll <= 18.8:
             summon_pull.append(("SR", random.choice(pool_sr)))
+
         else:
             summon_pull.append(("R", random.choice(pool_r)))
 
@@ -117,19 +157,25 @@ async def summon_perform(ctx, user, amulet_pull):
     for entry in summon_pull:
         description += ":small_orange_diamond:{}\n".format(entry[1])
 
-    embed = discord.Embed(color=user.colour, title=":confetti_ball: Results", description=description)
+    embed = discord.Embed(color=user.colour, title="🎊 Results", description=description)
 
     if amulet_pull == 10:
         embed.set_footer(text=f"{f_sp}; {f_ssr}; {f_sr}; {f_r}")
 
-    # Thumbnails
     elif amulet_pull == 1:
         rarity = summon_pull[0][0]
         shiki = summon_pull[0][1].replace("||", "")
-        thumbnail = \
-            shikigami.find_one({"rarity": rarity}, {"_id": 0, "shikigami": {"$elemMatch": {"name": shiki}}})[
-                "shikigami"][0]["thumbnail"]["pre_evo"]
-        embed.set_thumbnail(url=thumbnail)
+
+        thumbnail = shikigami.find_one({
+            "rarity": rarity}, {
+            "_id": 0, "shikigami": {
+                "$elemMatch": {
+                    "name": shiki
+                }
+            }
+        })
+
+        embed.set_thumbnail(url=thumbnail["shikigami"][0]["thumbnail"]["pre_evo"])
 
     msg = "{}".format(random.choice(summon_caption)).format(user.mention)
     await ctx.channel.send(msg, embed=embed)
@@ -146,10 +192,16 @@ class Summon(commands.Cog):
     @commands.guild_only()
     async def summon(self, ctx, arg):
         user = ctx.author
+
         try:
             amulet_pull = int(arg)
             amulet_have = users.find_one({"user_id": str(user.id)}, {"_id": 0, "amulets": 1})["amulets"]
-            if amulet_have > 0:
+
+            if amulet_have == 0:
+                await ctx.channel.send(f"{user.mention}, you have no {emoji_a} to summon")
+
+            elif amulet_have > 0:
+
                 if amulet_pull > amulet_have:
                     await ctx.channel.send(f"{user.mention}, you only have {amulet_have}{emoji_a} to summon")
 
@@ -158,12 +210,12 @@ class Summon(commands.Cog):
 
                 elif amulet_pull == 1 and amulet_have >= 1:
                     await summon_perform(ctx, user, amulet_pull)
+
                 else:
-                    await ctx.channel.send("{user.mention}, summon can only be by ones or by tens.")
-            else:
-                await ctx.channel.send(f"{user.mention}, you have no {emoji_a} to summon")
+                    await ctx.channel.send(f"{user.mention}, summon can only be by ones or by tens")
+
         except ValueError:
-            await ctx.channel.send("Type `;summon <1 or 10>` to perform summon.")
+            await ctx.channel.send("Type `;summon <1 or 10>` to perform summon")
 
 
 def setup(client):
