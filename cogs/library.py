fa276b0c9421838c4fb7ef2b739d2e52fcd930d5
kyrvscyl <51079653+kyrvscyl@users.noreply.github.com>
2019-07-10 23:53:27 +0800

    global deployment of library.py works on reference section

--- cogs/library.py
+++ cogs/library.py
@@ -17,22 +17,13 @@ tz_target = pytz.timezone("America/Atikokan")
 # Global variable
 lists_souls = []
 lists_souls_raw = []
-for soul in library.find({"book": "souls"}, {"_id": 0, "title": 1}):
-    lists_souls.append("`{}`".format(soul["title"].lower()))
-    lists_souls_raw.append(soul["title"].lower())
+for soul in library.find({"section": "sbs", "index": {"$nin": ["1", "2"]}}, {"_id": 0, "index": 1}):
+    lists_souls.append("`{}`".format(soul["index"].lower()))
+    lists_souls_raw.append(soul["index"].lower())
 
 
-# noinspection PyShadowingNames
-def check_if_none(embedding, parameter):
-    try:
-        value = embedding[parameter]
-        return value
-    except KeyError:
-        return None
-
-
-async def post_process_books(user, ctx):
-
+async def post_process_books(user, ctx, query):
+    library.update_one(query, {"$inc": {"borrows": 1}})
     profile = daily.find_one({"key": "library", f"{user.id}": {"$type": "int"}}, {"_id": 0, f"{user.id}": 1})
 
     if profile[f"{user.id}"] <= 3:
@@ -43,8 +34,11 @@ async def post_process_books(user, ctx):
                 f"{user.id}": 1
             }
         })
-
     await ctx.message.delete()
+
+
+def check_if_reference_section(ctx):
+    return ctx.channel.name == "reference-section"
 
 
 class Library(commands.Cog):
@@ -52,17 +46,84 @@ class Library(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    def create_webhook_post(self, webhooks, book):
+
+        bukkuman = webhooks[0]
+        webhook = DiscordWebhook(
+            content=book['content'],
+            url=bukkuman.url,
+            avatar_url="https://i.imgur.com/5FflHQ5.jpg",
+            username="Professor Bukkuman"
+        )
+
+        def generate_embed_value_1(dictionary, key):
+            try:
+                value = dictionary[key]
+            except KeyError:
+                value = None
+            return value
+
+        try:
+            for entry in book["embeds"]:
+                embed = DiscordEmbed(
+                    title=generate_embed_value_1(entry, "title"),
+                    description=generate_embed_value_1(entry, "description"),
+                    color=generate_embed_value_1(entry, "color"),
+                )
+
+                try:
+                    embed.set_thumbnail(url=generate_embed_value_1(entry, "thumbnail")["url"])
+                except TypeError:
+                    pass
+
+                try:
+                    embed.set_image(url=generate_embed_value_1(entry, "image")["url"])
+                except TypeError:
+                    pass
+
+                try:
+                    for field in entry["fields"]:
+                        print(field)
+                        embed.add_embed_field(
+                            name=field["name"],
+                            value=field["value"],
+                            inline=False
+                        )
+                except KeyError:
+                    pass
+
+                try:
+                    user_id = generate_embed_value_1(entry, "footer")["text"]
+                    user = self.client.get_user(int(user_id))
+                    embed.set_footer(
+                        text=f"Guide by {user}",
+                        icon_url=user.avatar_url
+                    )
+                except TypeError:
+                    pass
+                except AttributeError:
+                    pass
+
+                webhook.add_embed(embed)
+
+        except KeyError:
+            pass
+
+        return webhook
+
     @commands.command(aliases=["table"])
+    @commands.check(check_if_reference_section)
     async def post_table_of_content(self, ctx):
 
+        await ctx.message.delete()
         webhooks = await ctx.channel.webhooks()
         bukkuman = webhooks[0]
         webhook = DiscordWebhook(url=bukkuman.url, avatar_url="https://i.imgur.com/5FflHQ5.jpg")
 
         lists_souls_formatted = ", ".join(lists_souls)
         description = \
-            "• To open a book use `;open [field] [value]`\n" \
-            "• Example: `;open ab 3`"
+            "• To open a book use `;open [section] [index]`\n" \
+            "• Example: `;open sbs 3`"
 
         embed = DiscordEmbed(
             title=":bookmark: Table of Magical Contents",
@@ -70,24 +131,28 @@ class Library(commands.Cog):
             description=description
         )
         embed.add_embed_field(
-            name=":book: The Standard Book of Souls - Year 1 `[s1]`",
+            name=":book: The Standard Book of Souls - Year 1 `[SBS]`",
             value="{}".format(lists_souls_formatted)
         )
         embed.add_embed_field(
-            name=":book: The Standard Book of Souls - Year 5 `[s5]`",
-            value="• `[25]` Souls 10 Speed run (24-25s)\n"
-                  "• `[21]` Souls 10 Speed run (20-21s)"
+            name=":book: The Standard Book of Souls - Year 5 `[SBS]`",
+            value="• `[1]` Souls 10 Speed Run (24-25s)\n"
+                  "• `[2]` Souls 10 Speed Run (20-21s)\n"
+                  "• `[3]` Souls Moan Team Varieties"
         )
         embed.add_embed_field(
-            name=":books: Assorted Books `[ab]`",
+            name=":closed_book: Secret Duelling Books `[SDB]`",
+            value="• `[1]` Curses & Counter-Curses by zu(IA)uz - Book 1\n"
+                  "• `[2]` Curses & Counter-Curses by zu(IA)uz - Book 1\n"
+                  "• `[3]` What if by Quinlynn - Book 1\n"
+                  "• `[4]` What if by Quinlynn - Book 2"
+        )
+        embed.add_embed_field(
+            name=":books: Assorted Books `[AB]`",
             value="• `[1]` Advanced Realm-Making\n"
                   "• `[2]` A Beginner's Guide to Shikigami Affection\n"
                   "• `[3]` Predicting the Unpredictable: Summon Odds\n"
-                  "• `[4]` Curses & Counter-Curses by zu(IA)uz - Book 1\n"
-                  "• `[5]` Curses & Counter-Curses by zu(IA)uz - Book 1\n"
-                  "• `[6]` Spellman's Syllabary: Contractions\n"
-                  "• `[7]` What if by Quinlynn - Book 1\n"
-                  "• `[8]` What if by Quinlynn - Book 2\n"
+                  "• `[4]` Spellman's Syllabary: Contractions"
         )
         embed.set_footer(
             text="grants 50 exp per book opened (max of 150/day)"
@@ -96,146 +161,36 @@ class Library(commands.Cog):
         webhook.execute()
 
     @commands.command(aliases=["open"])
+    @commands.check(check_if_reference_section)
     async def post_book(self, ctx, arg1, *, args):
 
         webhooks = await ctx.channel.webhooks()
-        bukkuman = webhooks[0]
         user = ctx.message.author
+        query = {"section": arg1.lower(), "index": args.lower()}
+        book = library.find_one(query, {"_id": 0})
 
-        if arg1.lower() == "s1" and args.lower() in lists_souls_raw:
 
-            book = library.find_one({"title": args.title()}, {"_id": 0, "book": 0})
-            webhook = DiscordWebhook(url=bukkuman.url, avatar_url="https://i.imgur.com/5FflHQ5.jpg")
-
-            embed = DiscordEmbed(
-                title=book["title"],
-                color=book["color"]
-            )
-            embed.set_thumbnail(url=book["thumbnail"]["url"])
-
-            for field in book["fields"]:
-                embed.add_embed_field(
-                    name=field["name"],
-                    value=field["value"],
-                    inline=field["inline"]
-                )
-
-            webhook.add_embed(embed)
-            webhook.execute()
-            library.update_one({"title": args.title()}, {"$inc": {"borrows": 1}})
-            await post_process_books(user, ctx)
-
-        elif arg1.lower() == "s5" and args.lower() in ["21", "25"]:
-
-            book = library.find_one({"book": arg1.lower(), "chapter": args.lower()}, {"_id": 0, "book": 0})
-            webhook = DiscordWebhook(
-                url=bukkuman.url,
-                avatar_url="https://i.imgur.com/5FflHQ5.jpg",
-                content=book["title"]
-            )
-
-            for embedding in book["embeds"]:
-                title = check_if_none(embedding, "title")
-                color = check_if_none(embedding, "color")
-                description = check_if_none(embedding, "description")
-
-                try:
-                    image_url = check_if_none(embedding, "image")["url"]
-                except TypeError:
-                    image_url = None
-
-                embed = DiscordEmbed(
-                    title=title,
-                    color=color,
-                    description=description
-                )
-                embed.set_image(url=image_url)
-                webhook.add_embed(embed)
-
-            webhook.execute()
-            library.update_one({"book": arg1.lower(), "chapter": args.lower()}, {"$inc": {"borrows": 1}})
-            await post_process_books(user, ctx)
-
-        elif arg1.lower() == "ab" and args.lower() in ["1", "2", "6"]:
-
-            book = library.find_one({"book": "assorted", "chapter": args.lower()}, {"_id": 0, "book": 0})
-            webhook = DiscordWebhook(
-                content=f"📙 {book['title']}",
-                url=bukkuman.url,
-                avatar_url="https://i.imgur.com/5FflHQ5.jpg"
-            )
-
-            for embedding in book["embeds"]:
-
-                try:
-                    image_url = check_if_none(embedding, "image")["url"]
-                except TypeError:
-                    image_url = None
-
-                try:
-                    footer_text = check_if_none(embedding, "footer")["text"]
-                    footer_text = f"Guide contributed by {self.client.get_user(int(footer_text)).name}"
-                except TypeError:
-                    footer_text = None
-                except AttributeError:
-                    footer_text = None
-
-                try:
-                    footer_url = check_if_none(embedding, "footer")["icon_url"]
-                    footer_url = self.client.get_user(int(footer_url)).avatar_url
-                except TypeError:
-                    footer_url = None
-                except AttributeError:
-                    footer_url = None
-
-                embed = DiscordEmbed(
-                    title=check_if_none(embedding, "title"),
-                    color=check_if_none(embedding, "color"),
-                    description=check_if_none(embedding, "description")
-                )
-                embed.set_image(url=image_url)
-                embed.set_footer(
-                    text=footer_text,
-                    icon_url=footer_url
-                )
-                webhook.add_embed(embed)
-
-            webhook.execute()
-            library.update_one({"book": "assorted", "chapter": args.lower()}, {"$inc": {"borrows": 1}})
-            await post_process_books(user, ctx)
-
-        elif arg1.lower() == "ab" and args.lower() in ["4", "5", "7", "8"]:
-
-            book = library.find_one({"book": "assorted", "chapter": args.lower()}, {"_id": 0, "book": 0})
-            webhook = DiscordWebhook(
-                content=f"📕 {book['title']}\n{book['url']}",
-                url=bukkuman.url,
-                avatar_url="https://i.imgur.com/5FflHQ5.jpg"
-            )
-
-            webhook.execute()
-            library.update_one({"book": "assorted", "chapter": args.lower()}, {"$inc": {"borrows": 1}})
-            await post_process_books(user, ctx)
-
-        elif arg1.lower() == "ab" and args.lower() in ["3"]:
-
-            book = library.find_one({"book": "assorted", "chapter": args.lower()}, {"_id": 0})
-            link = book["attachment"]
-            contributor = book["user"]
+        if arg1.lower() in ["ab", "sbs"] and args.lower() in ["3"]:
 
             opener = urllib.request.build_opener()
             opener.addheaders = [('User-agent', 'Mozilla/5.0')]
             urllib.request.install_opener(opener)
-            urllib.request.urlretrieve(link, "data/SSR_rates.pdf")
+            urllib.request.urlretrieve(book["attachment"], book["address"])
 
-            file = discord.File("data/SSR_rates.pdf", filename="SSR_Rates.pdf")
+            file = discord.File(book["address"], filename=book["filename"])
+            contributor = self.client.get_user(int(book["contributor"]))
             await ctx.channel.send(
-                content="📙 {}".format(book["title"]).format(self.client.get_user(int(contributor))),
+                content=f"{book['content']} {contributor}",
                 file=file
             )
+            await post_process_books(user, ctx, query)
 
-            library.update_one({"book": "assorted", "chapter": args.lower()}, {"$inc": {"borrows": 1}})
-            await post_process_books(user, ctx)
+        elif arg1.lower() in ["sbs", "sdb", "ab"] and \
+                (args.lower() in lists_souls_raw or args.lower() in ["1", "2", "4"]):
+
+            webhook = self.create_webhook_post(webhooks, book)
+            webhook.execute()
+            await post_process_books(user, ctx, query)
 
 
 def setup(client):
