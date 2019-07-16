aba87f52908c49895d6ef20c39efdffb40b71a52
kyrvscyl <51079653+kyrvscyl@users.noreply.github.com>
2019-07-16 14:48:51 +0800

    added errors document

--- cogs/mongo/db.py
+++ cogs/mongo/db.py
@@ -4,11 +4,9 @@ kyrvscyl, 2019
 """
 from pymongo import MongoClient
 
-# Mongo Database Startup
 address = "mongodb+srv://headmaster:headmaster@memory-scrolls-uhsu0.mongodb.net/test?retryWrites=true&w=majority"
 memory = MongoClient(address)
 
-# Collections - Miketsu
 boss = memory["miketsu"]["boss"]
 compensation = memory["miketsu"]["compensation"]
 daily = memory["miketsu"]["daily"]
@@ -21,7 +19,6 @@ quests = memory["miketsu"]["quests"]
 owls = memory["miketsu"]["owls"]
 thieves = memory["miketsu"]["thieves"]
 
-# Collections - Bukkuman
 books = memory["bukkuman"]["books"]
 bounty = memory["bukkuman"]["bounty"]
 shikigamis = memory["bukkuman"]["shikigamis"]
@@ -29,3 +26,4 @@ members = memory["bukkuman"]["members"]
 weather = memory["bukkuman"]["weather"]
 patronus = memory["bukkuman"]["patronus"]
 library = memory["bukkuman"]["library"]
+errors = memory["bukkuman"]["errors"]
