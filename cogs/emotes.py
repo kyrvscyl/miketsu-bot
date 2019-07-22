2d9f64698a32cf7c4d6a43ca733e5ca1af4bf7cc
kyrvscyl <51079653+kyrvscyl@users.noreply.github.com>
2019-07-22 17:15:21 +0800

    massive embedifications & beautifications

--- cogs/emotes.py
+++ cogs/emotes.py
@@ -3,16 +3,12 @@ Discord Miketsu Bot.
 kyrvscyl, 2019
 """
 import json
-import os
 import random
 
 import discord
 from discord.ext import commands
 
-from cogs.error import logging, get_f
 from cogs.mongo.db import users
-
-file = os.path.basename(__file__)[:-3:]
 
 with open("data/emotes.json") as f:
     stickers = json.load(f)
@@ -32,15 +28,16 @@ class Emotes(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-
-    @commands.command(aliases=["sticker"])
+    @commands.command(aliases=["sticker", "stickers"])
     @commands.guild_only()
-    async def stickers(self, ctx):
-
-        embed = discord.Embed(color=0xffff80, title="Available stickers!", description=description)
-        embed.set_footer(text="just add 'mike' plus the sticker name in your message!")
+    async def sticker_help(self, ctx):
+        embed = discord.Embed(
+            color=0xffe6a7, title="stickers",
+            description="posts an reaction image embed\n"
+                        "just add \"mike\" in your message + the `<alias>`"
+        )
+        embed.add_field(name="Aliases", value="*" + description + "*")
         await ctx.channel.send(embed=embed)
-
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -51,69 +48,34 @@ class Emotes(commands.Cog):
         elif message.author.bot:
             return
 
+        elif isinstance(message.channel, discord.DMChannel):
+            return
+
         elif len(message.content.split(" ")) > 2:
             return
 
-        elif "mike" in message.content.lower():
+        elif "mike" in message.content.lower().split(" "):
             user = message.author
-            list_emotes = message.content.lower().split()
-            action_recognized = None
+            list_message = message.content.lower().split()
+            sticker_recognized = None
 
-            for action_try in list_emotes:
-                if action_try in actions:
-                    action_recognized = action_try
+            for sticker_try in list_message:
+                if sticker_try in actions:
+                    sticker_recognized = sticker_try
                     break
 
-            if action_recognized is None:
+            if sticker_recognized is None:
                 return
 
             else:
                 users.update_one({"user_id": str(user.id)}, {"$inc": {"experience": 20}})
-                selection = [v for v in emotes[action_recognized].values()]
-                thumbnail = random.choice(selection)
-                embed = discord.Embed(color=message.author.colour)
-                embed.set_footer(
-                    text=f"{message.author.display_name}, +20exp",
-                    icon_url=message.author.avatar_url
-                )
-                embed.set_image(url=thumbnail)
-                try:
-                    await message.channel.send(embed=embed)
-                except discord.errors.Forbidden:
-                    logging(file, get_f, "discord.errors.Forbidden")
-                    return
-                except discord.errors.HTTPException:
-                    logging(file, get_f, "discord.errors.Forbidden")
-                    return
-                try:
-                    await message.delete()
-                except discord.errors.Forbidden:
-                    logging(file, get_f, "discord.errors.Forbidden")
-                except discord.errors.HTTPException:
-                    logging(file, get_f, "discord.errors.Forbidden")
-
-
-        elif "@{}".format(self.client.user.id) in message.content:
-            try:
+                selection = [v for v in emotes[sticker_recognized].values()]
+                sticker_url = random.choice(selection)
+                embed = discord.Embed(color=user.colour)
+                embed.set_footer(text=f"{user.display_name}, +20exp", icon_url=user.avatar_url)
+                embed.set_image(url=sticker_url)
+                await message.channel.send(embed=embed)
                 await message.delete()
-            except discord.errors.Forbidden:
-                logging(file, get_f, "discord.errors.Forbidden")
-            except discord.errors.HTTPException:
-                logging(file, get_f, "discord.errors.Forbidden")
-
-            msg = "<:0_0:596721853448454203><:1_0:596721853305978882>" \
-                  "<:2_0:596721853528014880><:3_0:596721853259841564><:4_0:596721853779804219>\n" \
-                  "<:0_1:596721853775740948><:1_1:596721853205315596>" \
-                  "<:2_1:596721853465231362><:3_1:596721853716889641><:4_1:596721853691592705>\n" \
-                  "<:0_2:596721853569957918><:1_2:596721853624483840>" \
-                  "<:2_2:596721853561569280><:3_2:596721853620420627><:4_2:596721853612163152>"
-
-            try:
-                await message.channel.send(msg)
-            except discord.errors.Forbidden:
-                logging(file, get_f, "discord.errors.Forbidden")
-            except discord.errors.HTTPException:
-                logging(file, get_f, "discord.errors.Forbidden")
 
 
 def setup(client):
