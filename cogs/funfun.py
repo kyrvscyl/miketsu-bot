c13c5a899b4e8f0ef73f38d9e91eaeaa2cd030c1
kyrvscyl <51079653+kyrvscyl@users.noreply.github.com>
2019-07-23 12:08:50 +0800

    funfun 0.1

--- cogs/funfun.py
+++ cogs/funfun.py
@@ -8,6 +8,71 @@ import re
 import discord
 from discord.ext import commands
 
+reaction_list_ = open("lists/reactions.lists")
+reaction_list = reaction_list_.read().splitlines()
+reaction_list_.close()
+
+success_lists_ = open("lists/success.lists")
+success_lists = success_lists_.read().splitlines()
+success_lists_.close()
+
+failed_lists_ = open("lists/failed.lists")
+failed_lists = failed_lists_.read().splitlines()
+failed_lists_.close()
+
+
+async def mike_shoot(user, guild, channel, args):
+
+    msg_formatted = args.lower().split(" ")
+
+    for word in msg_formatted:
+
+        if re.match(r"^<@![0-9]+>$", word) or re.match(r"^<@[0-9]+>$", word):
+
+            user_id = re.sub("[<>@!]", "", word)
+            member = guild.get_member(int(user_id))
+
+            roll = random.randint(1, 100)
+            response = random.choice(success_lists).format(member.mention)
+            if roll >= 45:
+                response = random.choice(failed_lists).format(user.mention)
+
+            embed = discord.Embed(
+                color=member.colour,
+                description="\"*"+response+"*\""
+            )
+            await channel.send(embed=embed)
+            break
+
+
+async def mike_how_hot(guild, channel, msg):
+    msg_formatted = msg.lower().split(" ")
+
+    for word in msg_formatted:
+
+        if re.match(r"^<@![0-9]+>$", word) or re.match(r"^<@[0-9]+>$", word):
+
+            user_id = re.sub("[<>@!]", "", word)
+            random.seed(int(user_id))
+            member = guild.get_member(int(user_id))
+            r = random.randint(1, 100)
+            hot = r / 1.17
+
+            emoji = "💔"
+            if hot > 25:
+                emoji = "❤"
+            if hot > 50:
+                emoji = "💖"
+            if hot > 75:
+                emoji = "💞"
+
+            embed = discord.Embed(
+                color=member.colour,
+                title=f"**{member.display_name}** is **{hot:.2f}%** hot {emoji} today"
+            )
+            await channel.send(embed=embed)
+            break
+
 
 class Funfun(commands.Cog):
 
@@ -17,34 +82,28 @@ class Funfun(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
 
-        if "mike" in message.content.lower():
+        if self.client.user == message.author:
+            return
 
-            if "how hot" in message.content.lower():
-                msg_formatted = message.content.lower().split(" ")
+        elif message.author.bot:
+            return
 
-                for word in msg_formatted:
-                    if re.match(r"^<@![0-9]+>$", word) or re.match(r"^<@[0-9]+>$", word):
+        elif message.content.lower()[:4] == "mike":
 
-                        user_id = re.sub("[<>@!]", "", word)
-                        random.seed(int(user_id))
-                        member = message.guild.get_member(int(user_id))
-                        r = random.randint(1, 100)
-                        hot = r / 1.17
+            if len(message.content) == 4:
+                embed = discord.Embed(
+                    colour=discord.Colour(0xffe6a7),
+                    description="\"*"+random.choice(reaction_list)+"*\""
+                )
+                msg = await message.channel.send(embed=embed)
+                await msg.delete(delay=15)
+                await message.delete(delay=15)
 
-                        emoji = "💔"
-                        if hot > 25:
-                            emoji = "❤"
-                        if hot > 50:
-                            emoji = "💖"
-                        if hot > 75:
-                            emoji = "💞"
+            elif message.content.lower().split(" ", 2)[1] == "shoot":
+                await mike_shoot(message.author, message.guild, message.channel, message.content)
 
-                        embed = discord.Embed(
-                            color=message.author.colour,
-                            title=f"**{member.display_name}** is **{hot:.2f}%** hot {emoji}"
-                        )
-                        await message.channel.send(embed=embed)
-                        break
+            elif message.content.lower().split(" ", 1)[1][:7] == "how hot":
+                await mike_how_hot(message.guild, message.channel, message.content)
 
 
 def setup(client):
